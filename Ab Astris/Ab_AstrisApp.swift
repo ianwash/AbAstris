3cbf3a3114b1aa091fbc117e163aff32e7222234
ianwash <ianwash@live.unc.edu>
2023-01-17 16:51:39 -0500

    Initial Commit

---
+++ Ab Astris/Ab_AstrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ab_AstrisApp.swift
+//  Ab Astris
+//
+//  Created by Ian Washabaugh on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Ab_AstrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
